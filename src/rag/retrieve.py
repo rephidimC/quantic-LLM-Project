3c506a13b91282f8230c45220302e059392ade26
Victor Afolayan <adelekeafolayan@gmail.com>
2026-02-23 13:59:33 +0100

    :test_tube: test deployment owing to memory failure

--- src/rag/retrieve.py
+++ src/rag/retrieve.py
@@ -1,15 +1,15 @@
 from langchain_community.vectorstores import Chroma
 # from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.embeddings import HuggingFaceEmbeddings
+# from langchain_community.embeddings import HuggingFaceEmbeddings
 
 # Initialize embeddings
-embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+# embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 # Initialize Chroma vector store
 vectordb = Chroma(
     collection_name="policies",
     persist_directory="data/chroma",
-    embedding_function=embedding
+    # embedding_function=embedding
 )
 
 # Retrieval function
